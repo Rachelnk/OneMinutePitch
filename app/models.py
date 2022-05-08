7e401e4d993e59c531c54637c8e9fda267eae391
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-08 18:28:33 +0300

    Add comment class model.

--- app/models.py
+++ app/models.py
@@ -37,4 +37,23 @@ class Pitch(db.Model):
         pitches = Pitch.query.filter_by(category=category).all()
         return pitches
 
+class Comment(db.Model):
+    '''
+    This class will contain the schema for comments
+    '''
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+        return comments
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
     
