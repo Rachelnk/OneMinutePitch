37fdc787879d1ce5e7744fe4bd775e4ffaffa118
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-08 16:25:05 +0300

    Import SQLALchemy and create a db instance.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 # Initializing application
 
