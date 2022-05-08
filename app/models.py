b36b81e4753316012aca8eb00b77c0cb4f62606a
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-08 17:11:15 +0300

    Add Migrate and MigrateCommand class.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
@@ -6,6 +7,9 @@ class User(db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref='user',lazy = 'dynamic')
+    comments = db.relationship('Comment',backref='user',lazy='dynamic')
+
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
 
---
+++ manage.py
@@ -0,0 +1,4 @@
+from app import create_app,db
+# .....
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
