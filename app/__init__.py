38834831707cc239c06325fdf9d01e79ddd8f2dd
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-08 18:35:11 +0300

    Import LoginManager class from flask_login extension and create an instance of LoginManager class.

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,13 @@ from app.models import User
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 # Initializing application
