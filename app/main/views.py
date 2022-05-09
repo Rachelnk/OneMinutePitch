48b8db2d9f0ffb902ed91d56f7996616b275dfe1
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-09 06:56:02 +0300

    Import Pitch, comments and update profile forms.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 from flask import render_template, request,redirect,url_for
 from . import main
+from flask_login import login_required,current_user,login_user,logout_user
+from .forms import PitchdForm, CommentsForm, UpdateProfile
+
 
 @main.route('/')
 def index():
