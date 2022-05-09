096d969452d70ba095639307026a6eb7f5631fb8
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-09 10:57:46 +0300

    Create an email and username functions to check iif the username or the email already exists in the db.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,3 +14,11 @@ class RegForm(FlaskForm):
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username has already been taken")
