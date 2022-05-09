f1f3ba822d20f486ff4021ba15bd6833d4d215a4
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-09 08:16:27 +0300

    Add save and delete user functions in models.py.

--- app/models.py
+++ app/models.py
@@ -16,7 +16,24 @@ class User(db.Model, UserMixin):
     upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
 
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
     
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
 
     def __repr__(self):
         return f'User {self.username}'
