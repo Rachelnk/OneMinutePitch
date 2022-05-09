079f088489b5ee3ecc4ca11dbec41feda6e142c6
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-09 12:49:57 +0300

    Add UserTest class and setup method, initialize User model class.

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,3 +1,7 @@
 import unittest
 
-from app.models import 
+from app.models import User
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='ray',password='123rnk')
