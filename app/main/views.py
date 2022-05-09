fc87e03b41b30f77401348b9554f67aecb5e8248
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-09 07:25:46 +0300

    Add navigation bar with profile, register and login form and logout links.

--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,14 @@ from . import main
 from flask_login import login_required,current_user,login_user,logout_user
 from .forms import PitchdForm, CommentsForm, UpdateProfile
 from ..models import User, Comment, Pitch
+from .. import db
 
 
 @main.route('/')
 def index():
+  product_pitch = Pitch.get_pitch('Product')
+  interview_pitch = Pitch.get_pitch('Interview')
+  promotion_pitch = Pitch.get_pitch('Promotion')
+  pickup_pitch = Pitch.get_pitch('Pick-up')
   title = 'Home - Welcome to One Minute Pitch. '
-  return render_template('index.html', title = title)
+  return render_template('index.html', title = title, pitches=product_pitch,interview_pitch=interview_pitch,promotion_pitch=promotion_pitch,pickup_pitch=pickup_pitch)
