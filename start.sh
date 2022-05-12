30ecd1f6445d0e55b5479af26a7c74e28277a01d
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-12 22:00:31 +0300

    Edit start.sh.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 # export MAIL_USERNAME='rachael.kiarie@student.moringaschool.com'
-# export MAIL_PASSWORD='Iwillb3atura$$@666'
+# export MAIL_PASSWORD=''
 export SECRET_KEY='123qwerty'
 python3.8 manage.py server
